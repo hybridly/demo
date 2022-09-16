2f5acb5b1292f7964b9b50e7f7c05b7a2d78a18d
Enzo Innocenzi <enzo@innocenzi.dev>
2022-09-17 00:02:21 +0200

    feat: add favicon

--- resources/views/root.blade.php
+++ resources/views/root.blade.php
@@ -1,21 +1,18 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="flex flex-col">
-
-<head>
-		<meta charset="UTF-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		{{-- <link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet"> --}}
-		<link rel="preconnect" href="https://fonts.googleapis.com">
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-		<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,600&display=swap"
-				rel="stylesheet">
-		@vite
-</head>
-
-<body class="h-full bg-gray-50 antialiased">
-		@monolikit(class: 'h-full')
-</body>
-
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full flex flex-col">
+	<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<link rel="preconnect" href="https://fonts.googleapis.com">
+			<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+			<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,600&display=swap" rel="stylesheet">
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+			<link rel="manifest" href="/site.webmanifest">
+			@vite
+	</head>
+	<body class="h-full bg-gray-50 antialiased">
+			@monolikit(class: 'h-full')
+	</body>
 </html>
