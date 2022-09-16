e437e123a8100497acf44100937971ea1c3956d8
Enzo Innocenzi <enzo@innocenzi.dev>
2022-09-16 21:10:14 +0200

    feat: animate chirp list

--- resources/application/main.ts
+++ resources/application/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { initializeMonolikit } from 'monolikit/vue'
 import { createHead } from '@vueuse/head'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 import 'virtual:monolikit/router'
 import './tailwind.css'
 
@@ -9,6 +10,7 @@ initializeMonolikit({
 	pages: import.meta.glob('@/views/pages/**/*.vue', { eager: true }),
 	setup: ({ render, element, monolikit }) => createApp({ render })
 		.use(createHead())
+		.use(autoAnimatePlugin)
 		.use(monolikit)
 		.mount(element),
 })
