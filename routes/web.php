9609de95b77045354397ee37ca36ba30bdddb4f7
Enzo Innocenzi <enzo@innocenzi.dev>
2022-09-25 23:42:07 +0200

    fix: uniformize chirp urls

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ChirpController::class, 'index'])->name('index');
 Route::post('/chirps', [ChirpController::class, 'store'])->name('chirp.store');
-Route::get('/chirp/{chirp}', [ChirpController::class, 'show'])->name('chirp.show');
+Route::get('/chirps/{chirp}', [ChirpController::class, 'show'])->name('chirp.show');
 Route::post('/chirps/{chirp}/likes', LikeChirpController::class)->name('chirp.like');
 Route::delete('/chirps/{chirp}/likes', UnlikeChirpController::class)->name('chirp.unlike');
 Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy'])->name('chirp.destroy');
