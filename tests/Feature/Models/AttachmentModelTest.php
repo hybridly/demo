a0082dcf948008d5b48722df99b6639452b54584
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2022-09-25 13:18:55 +0330

    add attachment file deletion test

--- tests/Feature/Models/AttachmentModelTest.php
+++ tests/Feature/Models/AttachmentModelTest.php
@@ -2,6 +2,7 @@
 
 use App\Models\Attachment;
 use App\Models\Chirp;
+use Illuminate\Http\UploadedFile;
 
 it('has a chirp relationship', function () {
     $chirp = Chirp::factory()->create();
@@ -19,4 +20,20 @@ it('has a url attribute', function () {
     expect($attachment->url)
         ->toBeString()
         ->not->toBeEmpty();
+});
+
+it('deletes attachment file after model is deleted', function () {
+    Storage::fake('attachments');
+
+    $file = UploadedFile::fake()->image('fake.jpg')->store('', ['disk' => 'attachments']);
+
+    $attachment = Attachment::factory()->create([
+        'path' => $file,
+    ]);
+
+    Storage::disk('attachments')->assertExists($file);
+
+    $attachment->delete();
+
+    Storage::disk('attachments')->assertMissing($file);
 });
