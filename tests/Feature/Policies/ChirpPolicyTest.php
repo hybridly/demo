6d4327a0e0c7f31c02400bb68093952747e8cee4
Enzo Innocenzi <enzo@innocenzi.dev>
2022-09-26 00:17:53 +0200

    refactor: improve `ChirpPolicy` tests

--- tests/Feature/Policies/ChirpPolicyTest.php
+++ tests/Feature/Policies/ChirpPolicyTest.php
@@ -1,119 +1,105 @@
 <?php
 
-use App\Http\Controllers\ChirpController;
-use App\Http\Controllers\Like\LikeChirpController;
-use App\Http\Controllers\Like\UnlikeChirpController;
 use App\Models\Chirp;
-use App\Models\Like;
-use Illuminate\Testing\TestResponse;
 
-use function Pest\Laravel\delete;
-use function Pest\Laravel\get;
-use function Pest\Laravel\post;
+it('allows users to view any chirp', function () {
+    actingAsUser();
+    expect(Gate::allows('viewAny', Chirp::class))->toBeTrue();
+});
 
-use Symfony\Component\HttpFoundation\Response;
+it('forbids guests to view any chirp', function () {
+    expect(Gate::allows('viewAny', Chirp::class))->toBeFalse();
+});
 
-/*
-|--------------------------------------------------------------------------
-| Creating chirps
-|--------------------------------------------------------------------------
-*/
-
-test('any user can create chirps', function () {
+it('allows users to view a chirp', function () {
     actingAsUser();
 
-    $storeRequest = post(action([ChirpController::class, 'store'], [
-        'body' => 'random-body',
-    ]));
-    expect($storeRequest)->isSuccessfulOrRedirect();
+    $chirp = Chirp::factory()->create();
+
+    expect(Gate::allows('view', $chirp))->toBeTrue();
 });
 
-/*
-|--------------------------------------------------------------------------
-| Deleting chirps
-|--------------------------------------------------------------------------
-*/
+it('forbids guests to view a chirp', function () {
+    $chirp = Chirp::factory()->create();
 
-it('will not allow user to delete a chirp they dont own', function () {
+    expect(Gate::allows('view', $chirp))->toBeFalse();
+});
+
+it('allows users to create chirps', function () {
     actingAsUser();
+    expect(Gate::allows('create', Chirp::class))->toBeTrue();
+});
+
+it('forbids guests from creating chirps', function () {
+    expect(Gate::allows('create', Chirp::class))->toBeFalse();
+});
+
+it('forbids a user to delete a chirp they dont own', function () {
+    actingAsUser();
+
+    $user = user();
+    $chirp = Chirp::factory()->fromUser($user)->create();
+
+    expect(Gate::allows('delete', $chirp))->toBeFalse();
+});
+
+it('allows a user to delete a chirp they own', function () {
+    actingAsUser($user = user());
+
+    $chirp = Chirp::factory()->fromUser($user)->create();
+
+    expect(Gate::allows('delete', $chirp))->toBeTrue();
+});
+
+it('forbids guests to delete a chirp', function () {
     $chirp = Chirp::factory()->create();
 
-    $deleteRequest = delete(action([ChirpController::class, 'destroy'], $chirp));
-
-    expect($deleteRequest)->isForbidden();
+    expect(Gate::allows('delete', $chirp))->toBeFalse();
 });
 
-/*
-|--------------------------------------------------------------------------
-| Liking chirps
-|--------------------------------------------------------------------------
-*/
+it('allows users to like a chirp', function () {
+    actingAsUser();
 
-it('will not allow user to like a chirp twice', function () {
-    actingAsUser($user = user());
-    $chirp = Chirp::factory()
-        ->fromUser($user)
-        ->has(Like::factory()->byUser($user)->count(1))
-        ->create();
-
-    post(action(LikeChirpController::class, $chirp))
-        ->assertForbidden();
-});
-
-it('will allow user to like a chirp they have not yet liked before', function () {
-    actingAsUser($user = user());
-    $chirp = Chirp::factory()
-        ->fromUser($user)
-        ->create();
-
-    post(action(LikeChirpController::class, $chirp))
-        ->assertStatus(Response::HTTP_NO_CONTENT);
-});
-
-/*
-|--------------------------------------------------------------------------
-| Unliking chirps
-|--------------------------------------------------------------------------
-*/
-
-it('will allow user to unlike a chirp they liked before', function () {
-    actingAsUser($user = user());
-    $chirp = Chirp::factory()
-        ->fromUser($user)
-        ->has(Like::factory()->byUser($user)->count(1))
-        ->create();
-
-    delete(action(UnlikeChirpController::class, $chirp))
-        ->assertStatus(Response::HTTP_NO_CONTENT);
-});
-
-it('will not allow user to unlike a chirp they have not liked before', function () {
-    actingAsUser($user = user());
-    $chirp = Chirp::factory()
-        ->fromUser($user)
-        ->create();
-
-    delete(action(UnlikeChirpController::class, $chirp))
-        ->assertForbidden();
-});
-
-/*
-|--------------------------------------------------------------------------
-| Guest users
-|--------------------------------------------------------------------------
-*/
-
-it('will not allow guest users to manage chirps', function (Closure $request) {
     $chirp = Chirp::factory()->create();
 
-    expect($request($chirp))->isRedirectedToLogin();
-})->with([
-    'index' => fn () => fn (Chirp $chirp): TestResponse => get(action([ChirpController::class, 'index'])),
-    'show' => fn () => fn (Chirp $chirp): TestResponse => get(action([ChirpController::class, 'show'], $chirp)),
-    'store' => fn () => function (Chirp $chirp): TestResponse {
-        return post(action([ChirpController::class, 'store']));
-    },
-    'destroy' => fn () => function (Chirp $chirp): TestResponse {
-        return delete(action([ChirpController::class, 'destroy'], $chirp));
-    },
-]);
+    expect(Gate::allows('like', $chirp))->toBeTrue();
+});
+
+it('forbids guests to like a chirp', function () {
+    $chirp = Chirp::factory()->create();
+
+    expect(Gate::allows('like', $chirp))->toBeFalse();
+});
+
+it('forbids users to like a chirp twice', function () {
+    actingAsUser($user = user());
+
+    $chirp = Chirp::factory()
+        ->fromUser($user)
+        ->withLike($user)
+        ->create();
+
+    expect(Gate::allows('like', $chirp))->toBeFalse();
+});
+
+it('allows users to unlike a chirp they liked before', function () {
+    actingAsUser($user = user());
+
+    $chirp = Chirp::factory()
+        ->fromUser($user)
+        ->withLike($user)
+        ->create();
+
+    expect(Gate::allows('unlike', $chirp))->toBeTrue();
+});
+
+it('forbids users to unlike a chirp they did not like before', function () {
+    actingAsUser($user = user());
+
+    $chirp = Chirp::factory()
+        ->fromUser($user)
+        ->withLike()
+        ->create();
+
+    expect(Gate::allows('unlike', $chirp))->toBeFalse();
+});
