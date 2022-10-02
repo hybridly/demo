7bf0bec4d829b2edbeb933d0e9a5fe9aa1f774ec
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2022-10-02 16:16:33 +0330

    Add active links to sidebar menu

--- resources/views/layouts/default.vue
+++ resources/views/layouts/default.vue
@@ -1,37 +1,44 @@
 <script setup lang="ts">
-import { route } from 'monolikit/vue'
+import { route, useContext } from 'monolikit/vue'
 import BaseButton from '@/views/components/base/button.vue'
 
 useHead({
 	titleTemplate: (title) => `${title} - Blue Bird`,
 })
+
+console.log(useContext().value?.url)
 </script>
 
 <template>
 	<main class="relative flex items-start justify-center gap-10 p-5">
 		<aside class="sticky top-5 left-0 flex flex-col items-center">
-			<div class="flex flex-col items-center py-5 text-blue-50">
+			<div class="grid place-items-center py-5 text-blue-50">
 				<RouterLink href="/" class="text-blue-500 transition hover:text-blue-600">
 					<logo class="h-12 w-12" />
 				</RouterLink>
-				<menu class="mt-16 flex w-full flex-col items-center gap-12 rounded-3xl bg-blue-500 p-10">
-					<base-button class="hover:text-white">
+				<menu class="mt-16 grid w-full gap-8 rounded-3xl bg-blue-500 p-8">
+					<base-button class="h-12 w-12 rounded-xl bg-white/25 text-white">
 						<i-fluent-home-16-filled class="h-6 w-7" />
 					</base-button>
-					<base-button class="hover:text-white">
+					<base-button class="h-12 w-12 rounded-xl hover:text-white">
 						<i-ion-notifications class="h-6 w-7" />
 					</base-button>
-					<base-button class="hover:text-white">
+					<base-button class="h-12 w-12 rounded-xl hover:text-white">
 						<i-material-symbols-android-messages class="h-6 w-7" />
 					</base-button>
-					<base-button class="hover:text-white">
+					<base-button class="h-12 w-12 rounded-xl hover:text-white">
 						<i-material-symbols-bookmark class="h-6 w-7" />
 					</base-button>
-					<base-button class="hover:text-white">
-						<i-fa6-solid-user class="h-6 w-7" />
+					<base-button class="h-12 w-12 rounded-xl hover:text-white">
+						<i-fa6-solid-user class="text-lg" />
 					</base-button>
 					<RouterLink :href="route('logout')" method="POST" :as="BaseButton">
-						<i-material-symbols-logout-rounded class="scale-[-1] text-2xl" />
+						<div class="grid place-items-center space-y-2 pt-5 transition duration-300 hover:text-white">
+							<i-material-symbols-logout-rounded class="scale-[-1] text-2xl" />
+							<div class="text-xs uppercase tracking-wide">
+								Logout
+							</div>
+						</div>
 					</RouterLink>
 				</menu>
 			</div>
