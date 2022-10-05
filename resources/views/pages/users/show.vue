62be2724038477d89b58e4aa25ef533cb4aa95ab
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2022-10-05 20:48:51 +0330

    Add infinite scrolling

--- resources/views/pages/users/show.vue
+++ resources/views/pages/users/show.vue
@@ -1,15 +1,40 @@
 <script setup lang="ts">
-useHead({
-	title: 'User settings',
-})
+import { useAutoAnimate } from '@formkit/auto-animate/vue'
 
 const props = defineProps<{
 	user: App.Data.UserProfileData
 	chirps: Paginator<App.Data.ChirpData>
 }>()
 
+useHead({
+	title: props.user.display_name,
+})
+
+const [list, enableChirpAnimations] = useAutoAnimate()
+const canLoad = computed(() => !!props.chirps.meta?.next_page_url)
 const chirps = ref<App.Data.ChirpData[]>([...props.chirps.data])
 
+function loadMoreChirps() {
+	if (!canLoad.value) {
+		return
+	}
+
+	router.get(props.chirps.meta!.next_page_url!, {
+		preserveState: true,
+		preserveScroll: true,
+		preserveUrl: true,
+		only: ['chirps'],
+		hooks: {
+			before: () => enableChirpAnimations(false),
+			success: () => chirps.value.push(...props.chirps.data),
+			after: () => enableChirpAnimations(true),
+		},
+	})
+}
+
+useInfiniteScroll(window, loadMoreChirps, {
+	offset: { bottom: 300 },
+})
 </script>
 
 <template layout>
@@ -68,7 +93,7 @@ const chirps = ref<App.Data.ChirpData[]>([...props.chirps.data])
 				Likes
 			</div>
 		</div>
-		<div class="flex flex-1 flex-col gap-8 pt-5">
+		<div ref="list" class="flex flex-1 flex-col gap-8 pt-5">
 			<chirp
 				v-for="chirp in chirps"
 				:key="chirp.id"
