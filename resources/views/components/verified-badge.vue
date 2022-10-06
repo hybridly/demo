bcce0d257a417f60d27168dec05baaa3118398a4
HassanZahirnia <m.hassan.zahirnia@gmail.com>
2022-10-06 14:25:40 +0330

    Add verified badge component

---
+++ resources/views/components/verified-badge.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+	verified: string | null
+	size?: 'sm' | 'md'
+}>(), {
+	size: 'sm',
+})
+</script>
+
+<template>
+	<div v-if="verified" class="shrink-0">
+		<i-ic-baseline-verified
+			class="text-blue-400"
+			:class="{
+				'h-5 w-5': size === 'sm',
+				'h-6 w-6': size === 'md',
+			}"
+		/>
+	</div>
+</template>
