11b75dcee055dac4f4e4b0ab2547d8b1ae7a30e3
Enzo Innocenzi <enzo@innocenzi.dev>
2022-10-12 20:11:28 +0200

    refactor: use official Vite integration (#8)

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,24 +1,10 @@
 <?php
 
 use App\Models\User;
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
-use Illuminate\Foundation\Testing\TestCase;
 use Illuminate\Support\Collection;
-use Tests\CreatesApplication;
+use Tests\TestCase;
 
-/*
-|--------------------------------------------------------------------------
-| Test Case
-|--------------------------------------------------------------------------
-|
-| The closure you provide to your test functions is always bound to a specific PHPUnit test
-| case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
-| need to change it using the "uses()" function to bind a different classes or traits.
-|
-*/
-
-uses(TestCase::class, CreatesApplication::class, LazilyRefreshDatabase::class)
-    ->in('Feature', 'Unit');
+uses(TestCase::class)->in('Feature', 'Unit');
 
 /*
 |--------------------------------------------------------------------------
