9381fe9f4434a63b5af4c37d0deec6ff05c1b67a
Salim Absi <salimabsifull@gmail.com>
2023-03-16 12:51:01 +0200

    fix: properly paginate chirps (#17)
    Co-authored-by: Enzo Innocenzi <enzo@innocenzi.dev>

--- resources/utils/chirps.ts
+++ resources/utils/chirps.ts
@@ -2,22 +2,27 @@ import { useAutoAnimate } from '@formkit/auto-animate/vue'
 
 export function useInfiniteChirpLoading(initialChirps: Paginator<App.Data.ChirpData>) {
 	const [list, enableChirpAnimations] = useAutoAnimate()
-	const canLoad = computed(() => !!initialChirps.meta?.next_page_url)
-	const chirps = ref<App.Data.ChirpData[]>([...initialChirps.data])
+	const meta = ref(initialChirps.meta)
+	const chirps = ref(initialChirps.data)
+	const canLoad = computed(() => !!meta.value.next_page_url)
 
 	function loadMoreChirps() {
 		if (!canLoad.value) {
 			return
 		}
 
-		router.get(initialChirps.meta!.next_page_url!, {
+		router.get(meta.value.next_page_url!, {
 			preserveState: true,
 			preserveScroll: true,
 			preserveUrl: true,
 			only: ['chirps'],
 			hooks: {
 				before: () => enableChirpAnimations(false),
-				success: () => chirps.value.push(...initialChirps.data),
+				success: ({ view }) => {
+					const paginatedChirps = view.properties.chirps as unknown as Paginator<App.Data.ChirpData>
+					chirps.value.push(...paginatedChirps.data)
+					meta.value = paginatedChirps.meta
+				},
 				after: () => enableChirpAnimations(true),
 			},
 		})
