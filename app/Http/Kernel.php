9df0d5ee803bb61136ed5d9c1c395af45a0c694a
Enzo Innocenzi <enzo@innocenzi.dev>
2023-03-20 15:04:58 +0100

    chore: upgrade laravel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,7 @@ class Kernel extends HttpKernel
      *
      * @var array<string, class-string|string>
      */
-    protected $routeMiddleware = [
+    protected $middlewareAliases = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
