4d9743461ccbf93bf69ef98db88d7e02165e09bf
Enzo Innocenzi <enzo@innocenzi.dev>
2023-06-19 13:40:57 +0200

    chore: update dependencies

--- resources/application/main.ts
+++ resources/application/main.ts
@@ -2,7 +2,6 @@ import { initializeHybridly } from 'virtual:hybridly/config'
 import { createHead } from '@vueuse/head'
 import { autoAnimatePlugin as autoAnimate } from '@formkit/auto-animate/vue'
 import i18n from './i18n'
-import 'virtual:hybridly/router'
 import './tailwind.css'
 import './fonts'
 
